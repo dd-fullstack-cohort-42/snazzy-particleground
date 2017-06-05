094d9eb8156ef3bd1dfd6da0e59e76f5d96e34ba
Rochelle Lewis <rlewis37@gmail.com>
2017-06-05 15:57:32 -0600

    implement particleground

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- Custom CSS Goes HERE -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery - required for Bootstrap Components -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- particleground -->
+		<script src="js/jquery.particleground.min.js" type="text/javascript"></script>
+
+		<!-- custom js -->
+		<script src="js/custom.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Snazzy Particleground</title>
+	</head>
+	<body>
+
+		<section id="particleground">
+			<div id="welcome">
+				<h1>This is Particleground</h1>
+				<p class="lead">View on GitHub</p>
+			</div>
+		</section>
+
+		<section id="section-2">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12">
+						<h2>Section 2</h2>
+						<p class="lead">Tote bag shabby chic locavore lumbersexual umami, subway tile gastropub messenger bag pork belly. Kogi narwhal scenester af, austin raw denim neutra cronut swag brooklyn retro polaroid palo santo. Four dollar toast bushwick af skateboard etsy craft beer freegan, taiyaki slow-carb wolf. Normcore master cleanse street art, 90's tilde enamel pin VHS williamsburg 8-bit tumeric. Gentrify gochujang keffiyeh, kinfolk small batch hot chicken helvetica. Plaid live-edge salvia, vice normcore unicorn gastropub. Hot chicken af poutine, thundercats echo park migas fanny pack literally +1 tumblr stumptown raclette viral.</p>
+						<p class="lead">Hell of venmo schlitz stumptown edison bulb. Narwhal blue bottle fashion axe succulents occupy gluten-free. Mlkshk pour-over marfa, normcore raclette prism copper mug photo booth neutra kickstarter letterpress leggings put a bird on it tofu. Flexitarian vegan air plant williamsburg, etsy marfa gochujang. Cronut jianbing DIY occupy humblebrag hoodie. Microdosing shabby chic cred keytar, raclette farm-to-table forage. Listicle chicharrones scenester squid.</p>
+						<p class="lead">Tote bag shabby chic locavore lumbersexual umami, subway tile gastropub messenger bag pork belly. Kogi narwhal scenester af, austin raw denim neutra cronut swag brooklyn retro polaroid palo santo. Four dollar toast bushwick af skateboard etsy craft beer freegan, taiyaki slow-carb wolf. Normcore master cleanse street art, 90's tilde enamel pin VHS williamsburg 8-bit tumeric. Gentrify gochujang keffiyeh, kinfolk small batch hot chicken helvetica. Plaid live-edge salvia, vice normcore unicorn gastropub. Hot chicken af poutine, thundercats echo park migas fanny pack literally +1 tumblr stumptown raclette viral.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="section-3">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12 text-right">
+						<h2>Section 3</h2>
+						<p class="lead">Iceland fashion axe VHS, mlkshk keytar hell of air plant typewriter pickled bitters vice four loko whatever banjo. Intelligentsia twee jianbing cornhole lumbersexual, chillwave heirloom. Poutine occupy artisan tote bag jean shorts tumeric irony man braid, mixtape kogi. Aesthetic fanny pack quinoa tousled crucifix mumblecore shoreditch. PBR&B vaporware la croix, +1 leggings food truck palo santo iceland blue bottle lo-fi bushwick. Knausgaard listicle raclette sriracha selfies, fap helvetica. Fixie master cleanse forage, chicharrones tumeric glossier biodiesel wolf cred letterpress blue bottle.</p>
+						<p class="lead">Tumblr flannel vegan aesthetic, banjo hexagon direct trade gentrify organic austin chambray truffaut put a bird on it. VHS artisan taxidermy XOXO, glossier organic offal. Pinterest intelligentsia ugh irony polaroid tattooed ramps, pour-over kickstarter fixie heirloom kombucha meh. Keytar tofu paleo art party trust fund. Photo booth mustache hoodie schlitz, literally affogato master cleanse salvia. Whatever yr edison bulb occupy austin. Selvage activated charcoal vegan, slow-carb umami 8-bit four loko celiac quinoa kinfolk lyft selfies pickled messenger bag.</p>
+						<p class="lead">Tote bag shabby chic locavore lumbersexual umami, subway tile gastropub messenger bag pork belly. Kogi narwhal scenester af, austin raw denim neutra cronut swag brooklyn retro polaroid palo santo. Four dollar toast bushwick af skateboard etsy craft beer freegan, taiyaki slow-carb wolf. Normcore master cleanse street art, 90's tilde enamel pin VHS williamsburg 8-bit tumeric. Gentrify gochujang keffiyeh, kinfolk small batch hot chicken helvetica. Plaid live-edge salvia, vice normcore unicorn gastropub. Hot chicken af poutine, thundercats echo park migas fanny pack literally +1 tumblr stumptown raclette viral.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer>
+			<div class="container-fluid text-center">
+				This is the footer
+			</div>
+		</footer>
+
+	</body>
+</html>
---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	// initialize particleground on element by id
+	$("#particleground").particleground({
+		dotColor: '#5cbdaa',
+		lineColor: '#5cbdaa'
+	});
+
+});
